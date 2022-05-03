f16c1454f34084bc327b74002003149125386cbb
Pralhad Dhungana <pralhaddhungana@Pralhads-MacBook-Pro.local>
2022-05-03 17:06:16 +0545

    first commit

---
+++ frontend/js/query.js
@@ -0,0 +1,36 @@
+$(".slick-content").slick({
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+
+    ]
+});
+
+$(".slick-content-bottom").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
