92e35279d2d2869f6a7fc9639dd3acdddb862478
Pralhad Dhungana <pralhaddhungana@Pralhads-MacBook-Pro.local>
2022-05-19 18:12:39 +0545

    javascript

--- frontend/js/query.js
+++ frontend/js/query.js
@@ -1,3 +1,26 @@
+// function myFunction() {
+
+//     list = document.getElementById("form-inline-id");
+//     list.classList.add("mySearch");
+// }
+
+const navbarpane = document.getElementById("form-inline-id");
+const sticky = navbarpane.offsetTop;
+const overlay = document.getElementById("overlay-box");
+
+function myFunction() {
+    // alert("Hi there");
+    if (window.scrollY >= 282) {
+        navbarpane.classList.add("sticky");
+        overlay.classList.add("asd");
+    } else {
+        navbarpane.classList.remove("sticky");
+        overlay.classList.remove("asd");
+
+
+    }
+}
+
 $(".slick-content").slick({
     speed: 200,
     slidesToShow: 4,
