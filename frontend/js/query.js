369d871ce6be5ee9828b14519dca6ad353b42e83
Pralhad Dhungana <pralhaddhungana@Pralhads-MacBook-Pro.local>
2022-05-25 17:48:04 +0545

    update hotel page

--- frontend/js/query.js
+++ frontend/js/query.js
@@ -118,6 +118,35 @@ $(".slick-content").slick({
 
     ]
 });
+$(".related-hotels-items").slick({
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+
+    ]
+});
 
 $(".testimonial-slider").slick({
     arrow: true,
@@ -128,7 +157,7 @@ $(".testimonial-slider").slick({
     autoplaySpeed: 2000,
 });
 
-
+$("#target").zoom();
 
 
 
